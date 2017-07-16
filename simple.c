a1780277a29f32808556f59e0e69f863ba0d8a0d
Antonio Cuni <anto.cuni@gmail.com>
2017-07-16 18:21:42 +0200

    benchmark also other calling conventions

--- simple.c
+++ simple.c
@@ -5,8 +5,21 @@ static PyObject* noargs(PyObject* self, PyObject* args)
     Py_RETURN_NONE;
 }
 
+static PyObject* onearg(PyObject* self, PyObject* arg)
+{
+    Py_RETURN_NONE;
+}
+
+static PyObject* varargs(PyObject* self, PyObject* args)
+{
+    Py_RETURN_NONE;
+}
+
+
 static PyMethodDef SimpleMethods[] = {
     {"noargs", (PyCFunction)noargs, METH_NOARGS, ""},
+    {"onearg", (PyCFunction)onearg, METH_O, ""},
+    {"varargs", (PyCFunction)varargs, METH_VARARGS, ""},
     {NULL, NULL, 0, NULL}
 };
 
