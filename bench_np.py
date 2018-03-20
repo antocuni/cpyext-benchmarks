b7d33e9790158e406d1b900a22ac5e5b2e7b5b36
Antonio Cuni <anto.cuni@gmail.com>
2018-03-20 16:50:36 +0100

    another benchmark

--- bench_np.py
+++ bench_np.py
@@ -3,11 +3,18 @@ import numpy as np
 
 N = 100000
 
-def bench():
+def bench_getitem():
     myarray = np.zeros(10)
     with Timer('getitem'):
         total = 0
         for i in xrange(N):
             total += myarray[5]
 
-bench()
+def bench_mean():
+    myarray = np.array([12.34])
+    with Timer('np.mean'):
+        for i in xrange(N):
+            np.mean(myarray)
+
+bench_getitem()
+bench_mean()
